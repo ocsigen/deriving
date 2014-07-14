8070ef05b7b4d65d8d3d89f571727a16afe4c325
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-07-14 18:08:22 +0200

    Jenkins: build_doc.

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,5 +1,13 @@
+
 opam pin add --no-action deriving .
 opam install type_conv
 opam install --deps-only deriving
 opam install --verbose deriving
-opam remove --verbose deriving
+
+do_build_doc () {
+
+}
+
+do_remove () {
+  opam remove --verbose deriving
+}
