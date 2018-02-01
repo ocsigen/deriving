d993eddc5cf06330b419b055518d271a554ea0e4
Vasilis Papavasileiou <vasilis+git@besport.com>
2018-02-01 11:37:33 +0100

    Declare num ocamlfind dependency, call it 0.8.1

--- setup.ml
+++ setup.ml
@@ -1,10 +1,10 @@
 
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 172e37fc4b327922311f6cf9389bc560) *)
+(* DO NOT EDIT (digest: 1bc19e72587da58c1e1f99f847b509aa) *)
 (******************************************************************************)
 (* OASIS: architecture for building OCaml libraries and applications          *)
 (*                                                                            *)
-(* Copyright (C) 2011-2013, Sylvain Le Gall                                   *)
+(* Copyright (C) 2011-2016, Sylvain Le Gall                                   *)
 (* Copyright (C) 2008-2011, OCamlCore SARL                                    *)
 (*                                                                            *)
 (* This library is free software; you can redistribute it and/or modify it    *)
@@ -25,6 +25,8 @@
 
 open OASISDynRun
 
+let setup_t = BaseCompat.Compat_0_4.adapt_setup_t setup_t
+open BaseCompat.Compat_0_4
 (* OASIS_STOP *)
 
 let () = setup ();;
